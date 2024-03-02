6fab0ebca226289f9f84894bc3948d999b299e9d
amirulacacia <amirulacacia@outlook.com>
2024-03-02 18:24:35 +0800

    Adding CrossOrigin

--- src/main/java/com/amirul/spring/springbootmysql/controller/UserController.java
+++ src/main/java/com/amirul/spring/springbootmysql/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.amirul.spring.springbootmysql.model.User;
 import com.amirul.spring.springbootmysql.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 public class UserController {
